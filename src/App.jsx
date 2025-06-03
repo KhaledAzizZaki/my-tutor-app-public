1ebb29805339c43f0b9b2bc0b82718e9484204ac
Khaled Aziz Zaki <khaledazizzaki@gmail.com>
2025-06-03 23:47:42 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="font-roboto">
+      <h1 className="text-5xl">hello</h1>
+      <button className="btn btn-primary">click</button>
+    </div>
+  );
+}
+
+export default App;
