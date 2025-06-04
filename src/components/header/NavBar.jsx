4694554891e0d74a6fbd86c8d5bae4b6932e60e1
Khaled Aziz Zaki <khaledazizzaki@gmail.com>
2025-06-04 13:05:38 +0600

    feat: add navbar and footer

---
+++ src/components/header/NavBar.jsx
@@ -0,0 +1,78 @@
+import React from "react";
+import { NavLink } from "react-router";
+
+const NavBar = () => {
+  const link = (
+    <>
+      <li>
+        <NavLink to={"/"}>Home</NavLink>
+      </li>
+      <li>
+        <NavLink to={"/findTutors"}>Find tutors</NavLink>
+      </li>
+      <li>
+        <NavLink to={"/addTutor"}>Add Tutor</NavLink>
+      </li>
+      <li>
+        <NavLink to={"/myTutor"}>My Tutor</NavLink>
+      </li>
+      <li>
+        <NavLink to={"/myBookedTutors"}>My booked tutors</NavLink>
+      </li>
+      <li>
+        <NavLink to={"/login"}>Login</NavLink>
+      </li>
+      <li>
+        <NavLink to={"/registration"}>Registration</NavLink>
+      </li>
+      <li>
+        <NavLink to={"/logout"}>Logout</NavLink>
+      </li>
+    </>
+  );
+
+  return (
+    <div className="navbar bg-base-100 shadow-sm">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              {" "}
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />{" "}
+            </svg>
+          </div>
+
+          <ul
+            tabIndex={0}
+            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
+          >
+            {link}
+          </ul>
+        </div>
+
+        <h2 className="btn btn-ghost text-xl">TalkTutor</h2>
+      </div>
+
+      <div className="navbar-end">
+        <div className="hidden lg:flex">
+          <ul className="menu menu-horizontal px-1">{link}</ul>
+        </div>
+
+        <a className="btn">Button</a>
+      </div>
+    </div>
+  );
+};
+
+export default NavBar;
--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,10 +1,19 @@
 import React from "react";
 import App from "../App";
+import NavBar from "../components/header/NavBar";
+import { Outlet } from "react-router";
+import Footer from "../components/footer/Footer";
 
 const MainLayout = () => {
   return (
     <div>
-      <h1>this is main layout</h1>
+      <NavBar></NavBar>
+
+      <div className="min-h-[calc(100vh-300px)]">
+        <Outlet></Outlet>
+      </div>
+
+      <Footer></Footer>
     </div>
   );
 };
