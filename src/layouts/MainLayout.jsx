1eb387d57a56e66aae926dbd89a140e6ceb430f0
Khaled Aziz Zaki <khaledazizzaki@gmail.com>
2025-06-04 12:35:10 +0600

    feat: add router

---
+++ src/layouts/MainLayout.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import App from "../App";
+
+const MainLayout = () => {
+  return (
+    <div>
+      <h1>this is main layout</h1>
+    </div>
+  );
+};
+
+export default MainLayout;
