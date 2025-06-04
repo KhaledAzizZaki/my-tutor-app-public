b14e0edb32a94f7e20b1edb83d064983db42e226
Khaled Aziz Zaki <khaledazizzaki@gmail.com>
2025-06-04 12:18:19 +0600

    feat: add theme toggle button

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import "./App.css";
+import ThemeToggle from "./components/button/ThemeToggle";
 
 function App() {
   return (
-    <div className="font-roboto">
-      <h1 className="text-5xl">hello</h1>
-      <button className="btn btn-primary">click</button>
+    <div className="bg-white text-black dark:bg-black dark:text-white min-h-screen flex flex-col items-center justify-center">
+      <ThemeToggle />
+      <h1 className="text-3xl mt-4">Toggle Black & White Theme</h1>
     </div>
   );
 }
