b242a81877d3ebc3ad856b65b42ef3ecc7a63904
Khaled Aziz Zaki <khaledazizzaki@gmail.com>
2025-06-12 14:46:56 +0600

    feat: add delete button and connect to server API

--- src/pages/MyTutor.jsx
+++ src/pages/MyTutor.jsx
@@ -1,11 +1,44 @@
-import React from "react";
-import { useLoaderData } from "react-router";
+import { Trash2 } from "lucide-react";
+import React, { useState } from "react";
+import { Link, useLoaderData } from "react-router";
+import Swal from "sweetalert2";
+import useAxiosSecure from "../hooks/useAxiosSecure";
 
 const MyTutor = () => {
   const data = useLoaderData();
-  const myTutor = data.data;
+  const [myTutor, setMyTutor] = useState(data.data);
+
+  const axiosSecure = useAxiosSecure();
 
   //   console.log(data.data);
+
+  const handleDelete = (id) => {
+    console.log(id);
+
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        axiosSecure.delete(`/delete-tutor/${id}`).then((data) => {
+          if (data.data.deletedCount) {
+            Swal.fire({
+              title: "Deleted!",
+              text: "Your tutor has been deleted.",
+              icon: "success",
+            });
+            const remainingTutor = myTutor.filter((task) => task._id !== id);
+            setMyTutor(remainingTutor);
+          }
+        });
+      }
+    });
+  };
 
   return (
     <div>
@@ -35,7 +68,11 @@ const MyTutor = () => {
                   <td>{tutor.description}</td>
                   <td>{tutor.price}</td>
                   <td>{tutor.review}</td>
-                  <td className="flex flex-wrap items-center gap-2"></td>
+                  <td className="flex flex-wrap items-center gap-2">
+                    <Link onClick={() => handleDelete(tutor._id)}>
+                      <Trash2 />
+                    </Link>
+                  </td>
                 </tr>
               ))}
             </tbody>
