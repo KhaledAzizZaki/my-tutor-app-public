f5d3d5d2177a17d52fb1f31001db4c2cab12d5de
Khaled Aziz Zaki <khaledazizzaki@gmail.com>
2025-06-16 18:21:47 +0600

    style: footer design

--- src/components/bookingPlatform/Start.jsx
+++ src/components/bookingPlatform/Start.jsx
@@ -2,7 +2,12 @@ import React from "react";
 
 const Start = () => {
   return (
-    <div className="bg-white dark:bg-gray-800 mb-20">
+    <div
+      className="bg-white dark:bg-gray-800 mb-20  bg-cover bg-no-repeat w-full"
+      style={{
+        backgroundImage: `url("https://i.ibb.co/bMcDCcZp/Screenshot-2025-06-16-at-5-40-50-PM.png")`,
+      }}
+    >
       <div className="w-11/12 mx-auto py-20 md:flex justify-between">
         <div>
           <h4 className="text-3xl font-bold pb-5">
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,47 +1,82 @@
 import React from "react";
+import { CiFacebook } from "react-icons/ci";
+import {
+  FaFacebook,
+  FaInstagramSquare,
+  FaLinkedin,
+  FaTwitter,
+  FaYoutube,
+} from "react-icons/fa";
 
 const Footer = () => {
   return (
-    <footer className="footer sm:footer-horizontal bg-base-200 text-base-content p-10">
-      <aside>
-        <svg
-          width="50"
-          height="50"
-          viewBox="0 0 24 24"
-          xmlns="http://www.w3.org/2000/svg"
-          fillRule="evenodd"
-          clipRule="evenodd"
-          className="fill-current"
-        >
-          <path d="M22.672 15.226l-2.432.811.841 2.515c.33 1.019-.209 2.127-1.23 2.456-1.15.325-2.148-.321-2.463-1.226l-.84-2.518-5.013 1.677.84 2.517c.391 1.203-.434 2.542-1.831 2.542-.88 0-1.601-.564-1.86-1.314l-.842-2.516-2.431.809c-1.135.328-2.145-.317-2.463-1.229-.329-1.018.211-2.127 1.231-2.456l2.432-.809-1.621-4.823-2.432.808c-1.355.384-2.558-.59-2.558-1.839 0-.817.509-1.582 1.327-1.846l2.433-.809-.842-2.515c-.33-1.02.211-2.129 1.232-2.458 1.02-.329 2.13.209 2.461 1.229l.842 2.515 5.011-1.677-.839-2.517c-.403-1.238.484-2.553 1.843-2.553.819 0 1.585.509 1.85 1.326l.841 2.517 2.431-.81c1.02-.33 2.131.211 2.461 1.229.332 1.018-.21 2.126-1.23 2.456l-2.433.809 1.622 4.823 2.433-.809c1.242-.401 2.557.484 2.557 1.838 0 .819-.51 1.583-1.328 1.847m-8.992-6.428l-5.01 1.675 1.619 4.828 5.011-1.674-1.62-4.829z"></path>
-        </svg>
-        <p>
-          ACME Industries Ltd.
-          <br />
-          Providing reliable tech since 1992
-        </p>
-      </aside>
-      <nav>
-        <h6 className="footer-title">Services</h6>
-        <a className="link link-hover">Branding</a>
-        <a className="link link-hover">Design</a>
-        <a className="link link-hover">Marketing</a>
-        <a className="link link-hover">Advertisement</a>
-      </nav>
-      <nav>
-        <h6 className="footer-title">Company</h6>
-        <a className="link link-hover">About us</a>
-        <a className="link link-hover">Contact</a>
-        <a className="link link-hover">Jobs</a>
-        <a className="link link-hover">Press kit</a>
-      </nav>
-      <nav>
-        <h6 className="footer-title">Legal</h6>
-        <a className="link link-hover">Terms of use</a>
-        <a className="link link-hover">Privacy policy</a>
-        <a className="link link-hover">Cookie policy</a>
-      </nav>
-    </footer>
+    <div className="bg-blue-800">
+      <footer className="footer sm:footer-horizontal  text-white py-20 w-11/12 mx-auto px-0">
+        <aside>
+          <h2 className="font-bold text-2xl">TalkTutor</h2>
+          <p className="py-5">
+            Subscribe to our newsletter for the latest updates on features and
+            releases.
+          </p>
+          <div className="flex">
+            <input
+              type="text"
+              placeholder="Your email here"
+              className="border px-5 py-2"
+            />
+            <button className="px-5 py-2 bg-white text-black">Join</button>
+          </div>
+          <p className="text-[12px]">
+            By subscribing, you consent to our Privacy Policy and agree to
+            receive updates.
+          </p>
+        </aside>
+        <nav>
+          <h6 className="footer-title">Useful Links</h6>
+          <a className="link link-hover">About Us</a>
+          <a className="link link-hover">Contact Us</a>
+          <a className="link link-hover">Blog Posts</a>
+          <a className="link link-hover">Help Center</a>
+        </nav>
+        <nav>
+          <h6 className="footer-title">Connect With Us</h6>
+          <a className="link link-hover">Support</a>
+          <a className="link link-hover">Community</a>
+          <a className="link link-hover">Careers</a>
+          <a className="link link-hover">Partnerships</a>
+        </nav>
+        <nav>
+          <h6 className="footer-title">Follow Us</h6>
+          <a className="link link-hover flex gap-3">
+            <FaFacebook size={20} />
+            Facebook
+          </a>
+          <a className="link link-hover flex gap-3">
+            <FaInstagramSquare size={20} />
+            Instagram
+          </a>
+          <a className="link link-hover flex gap-3">
+            <FaTwitter size={20} />X
+          </a>
+          <a className="link link-hover flex gap-3">
+            <FaLinkedin size={20} />
+            LinkedIn
+          </a>
+          <a className="link link-hover flex gap-3">
+            <FaYoutube size={20} />
+            YouTube
+          </a>
+        </nav>
+      </footer>
+      <div className="text-white flex w-11/12 mx-auto gap-10 flex-wrap justify-between pb-10">
+        <p>© 2025 TalkTutor. All rights reserved.</p>
+        <div className="flex gap-5">
+          <a className="link link-hover underline">Privacy Policy</a>
+          <a className="link link-hover underline">Terms of Service</a>
+          <a className="link link-hover underline">Cookie Settings</a>
+        </div>
+      </div>
+    </div>
   );
 };
 
